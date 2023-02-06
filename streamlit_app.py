df992c362e2c8ac7ace4f4203df8aa8ecb32436b
suz-snowflake <124262222+suz-snowflake@users.noreply.github.com>
2023-02-06 10:27:18 +0100

    Update streamlit_app.py
    body displayed

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,6 @@ import streamlit
 import pandas
 import requests
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
-
 streamlit.title('My Mom\'s New Healthy Diner')
 
 streamlit.header('Breakfast Favorites')
@@ -22,3 +19,8 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
+
+streamlit.header('Fruityvice Fruit Advice!')
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response.json())
